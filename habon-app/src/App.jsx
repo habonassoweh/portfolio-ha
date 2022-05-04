34c97347cbe29629c80eaf9b8ef5f67abc92dd50
Habon Assoweh <habonassoweh@Habons-Air>
2022-05-04 10:05:42 -0400

    eits

--- habon-app/src/App.jsx
+++ habon-app/src/App.jsx
@@ -1,48 +1,73 @@
-import React from "react"
+import React from "react";
 import "./App.scss";
-
-
-import {Login} from './components/Login'
+import { Login } from "./components/Login";
 import { Signup } from "./components/Signup";
 
-class App extends React.Component{
-  constructor(props){
+class App extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
-      isLogginActive: true,
-    }
+      isLogginActive: true
+    };
   }
-   changeState(){
-     const { isLogginActive } = this.state;
-    //  is(isLogginActive)
 
-   }
+  componentDidMount() {
+    //Add .right by default
+    this.rightSide.classList.add("right");
+  }
 
-  render(){
-    const { isLogginActive } = this.state; 
-    const current = isLogginActive ? 'Signup': 'Login';
-    const currentActive = isLogginActive ? 'login': 'signup';
-    return(
+  changeState() {
+    const { isLogginActive } = this.state;
+
+    if (isLogginActive) {
+      this.rightSide.classList.remove("right");
+      this.rightSide.classList.add("left");
+    } else {
+      this.rightSide.classList.remove("left");
+      this.rightSide.classList.add("right");
+    }
+    this.setState(prevState => ({ isLogginActive: !prevState.isLogginActive }));
+  }
+
+  render() {
+    const { isLogginActive } = this.state;
+    const current = isLogginActive ? "Signup" : "Login";
+    const currentActive = isLogginActive ? "login" : "signup";
+    return (
       <div className="App">
         <div className="login">
-          <div className="container">
-             {isLogginActive && <Login containerRef ={ref => this.current = ref }/>}
-             {!isLogginActive && <Signup containerRef ={ref => this.current = ref} />}
+          <div className="container" ref={ref => (this.container = ref)}>
+            {isLogginActive && (
+              <Login containerRef={ref => (this.current = ref)} />
+            )}
+            {!isLogginActive && (
+              <Signup containerRef={ref => (this.current = ref)} />
+            )}
           </div>
-          <RightSide current={current} containerRef ={ref => this.rightSide = ref} onClick={this.changeStat.bin(this)}/>
+          <RightSide
+            current={current}
+            currentActive={currentActive}
+            containerRef={ref => (this.rightSide = ref)}
+            onClick={this.changeState.bind(this)}
+          />
         </div>
       </div>
-    )
+    );
   }
 }
 
-
 const RightSide = props => {
-  return <div className="right-side" ref={props.containerRef} onClick={props.onClick}>
-    <div className="inner-container">
-      <div className="text">{props.current}</div>
+  return (
+    <div
+      className="right-side"
+      ref={props.containerRef}
+      onClick={props.onClick}
+    >
+      <div className="inner-container">
+        <div className="text">{props.current}</div>
+      </div>
     </div>
+  );
+};
 
-  </div>
-}
 export default App;
